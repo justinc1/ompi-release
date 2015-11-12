5b0d50207c0d8764405e9f1e73e55ebdc1bfb91e
Justin Cinkelj <justin.cinkelj@xlab.si>
2015-11-12 18:46:00 +0100

    Just a script to build orted.so.

---
+++ make-orted-so.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# cd build
+# cd orte/tools/orted
+# make V=1 orted
+#
+#depbase=`echo orted.o | sed 's|[^/]*$|.deps/&|;s|\.o$||'`;\
+#	gcc -std=gnu99 -DHAVE_CONFIG_H -I. -I../../../../orte/tools/orted -I../../../opal/include -I../../../orte/include -I../../../ompi/include -I../../../oshmem/include -I../../../opal/mca/hwloc/hwloc191/hwloc/include/private/autogen -I../../../opal/mca/hwloc/hwloc191/hwloc/include/hwloc/autogen   -I../../../.. -I../../.. -I../../../../opal/include -I../../../../orte/include -I../../../../ompi/include -I../../../../oshmem/include   -I/home/justin_cinkelj/devel/mikelangelo/ompi-release/opal/mca/event/libevent2021/libevent -I/home/justin_cinkelj/devel/mikelangelo/ompi-release/opal/mca/event/libevent2021/libevent/include -I/home/justin_cinkelj/devel/mikelangelo/ompi-release/build-osv/opal/mca/event/libevent2021/libevent/include  -fPIC -DPIC -g -finline-functions -fno-strict-aliasing -pthread -MT orted.o -MD -MP -MF $depbase.Tpo -c -o orted.o ../../../../orte/tools/orted/orted.c &&\
+#	mv -f $depbase.Tpo $depbase.Po
+#/bin/bash ../../../libtool  --tag=CC   --mode=link gcc -std=gnu99  -fPIC -DPIC -g -finline-functions -fno-strict-aliasing -pthread   -o orted orted.o ../../../orte/libopen-rte.la ../../../opal/libopen-pal.la -lrt -lm -lutil  
+#libtool: link: gcc -std=gnu99 -fPIC -DPIC -g -finline-functions -fno-strict-aliasing -pthread -o .libs/orted orted.o  ../../../orte/.libs/libopen-rte.so ../../../opal/.libs/libopen-pal.so -lrt -lm -lutil -pthread
+
+
+#cd build-osv/orte/tools/orted/
+pushd           orte/tools/orted/
+
+rm orted orted.o orted.so .libs/orted .libs/orted.so
+make orted
+gcc -std=gnu99 -fPIC -DPIC -g -finline-functions -fno-strict-aliasing -pthread -o .libs/orted.so orted.o  ../../../orte/.libs/libopen-rte.so ../../../opal/.libs/libopen-pal.so -lrt -lm -lutil -pthread  -shared -fPIC
+
+popd
+
+ls -la orte/tools/orted/.libs/orted.so
